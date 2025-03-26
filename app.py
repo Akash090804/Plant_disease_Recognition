259a55b9e849277f7cdb24a60441649b2dec5ee3
Akash Varshney <Akashvarshney65025@gmail.com>
2025-03-27 00:08:47 +0530

    Space commit

--- app.py
+++ app.py
@@ -21,36 +21,12 @@ if not os.path.exists(MODEL_PATH):
 
 model = tf.keras.models.load_model(MODEL_PATH)
 
-label = ['Apple__Apple_scab', 'Apple_Black_rot', 'Apple_Cedar_apple_rust', 'Apple__healthy',
- 'Background_without_leaves', 'Blueberry__healthy', 'Cherry_Powdery_mildew', 'Cherry__healthy',
- 'Corn__Cercospora_leaf_spot Gray_leaf_spot', 'Corn_Common_rust', 'Corn__Northern_Leaf_Blight',
- 'Corn__healthy', 'Grape_Black_rot', 'Grape_Esca(Black_Measles)', 'Grape__Leaf_blight(Isariopsis_Leaf_Spot)',
- 'Grape__healthy', 'Orange_Haunglongbing(Citrus_greening)', 'Peach__Bacterial_spot', 'Peach__healthy',
- 'Pepper,bell_Bacterial_spot', 'Pepper,_bell_healthy', 'Potato_Early_blight', 'Potato__Late_blight',
- 'Potato__healthy', 'Raspberry_healthy', 'Soybean_healthy', 'Squash__Powdery_mildew',
- 'Strawberry__Leaf_scorch', 'Strawberry_healthy', 'Tomato_Bacterial_spot', 'Tomato__Early_blight',
- 'Tomato__Late_blight', 'Tomato_Leaf_Mold', 'Tomato__Septoria_leaf_spot',
- 'Tomato__Spider_mites Two-spotted_spider_mite', 'Tomato__Target_Spot',
- 'Tomato__Tomato_Yellow_Leaf_Curl_Virus', 'Tomato_Tomato_mosaic_virus', 'Tomato__healthy']
-
 with open("plant_disease.json", 'r') as file:
     plant_disease = json.load(file)
 
-
-
-
-@app.route('/uploadimages/<path:filename>')
-def uploaded_images(filename):
-    return send_from_directory('./uploadimages', filename)
-
-
-
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def home():
-    return render_template('home.html')
-
-
-
+    return render_template("home.html")
 
 def extract_features(image):
     image = tf.keras.utils.load_img(image, target_size=(160, 160))
@@ -58,34 +34,20 @@ def extract_features(image):
     feature = np.array([feature])
     return feature
 
-
-
-
 def model_predict(image):
     img = extract_features(image)
     prediction = model.predict(img)
     prediction_label = plant_disease[prediction.argmax()]
     return prediction_label
 
-
-
-@app.route('/upload/', methods=['POST', 'GET'])
+@app.route("/upload/", methods=["POST"])
 def uploadimage():
-    if request.method == "POST":
-        image = request.files['img']
-        temp_name = f"uploadimages/temp_{uuid.uuid4().hex}"
-        image_path = f"{temp_name}_{image.filename}"
-        image.save(image_path)
-        prediction = model_predict(image_path)
-        return render_template('home.html', result=True, imagepath=f'/{image_path}', prediction=prediction)
-    else:
-        return redirect('/')
-
-
-
-
-
+    image = request.files["img"]
+    temp_name = f"uploadimages/temp_{uuid.uuid4().hex}"
+    image_path = f"{temp_name}_{image.filename}"
+    image.save(image_path)
+    prediction = model_predict(image_path)
+    return render_template("home.html", result=True, imagepath=f"/{image_path}", prediction=prediction)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",
-            port=10000,debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 7860)))
